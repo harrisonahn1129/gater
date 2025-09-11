e449199f9a927954bf1bf005bb04099f03707c34
Jeremy Muhlich <jmuhlich@bitflood.org>
2025-09-10 21:04:57 -0400

    Refactor run.py for use as the pyoxidizer entry point

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from waitress import serve
 
 from minerva_analysis import app
 
-if __name__ == '__main__':
+def main():
     # use port 8000 if no port is specified via command line argument
     port = 8000 if len(sys.argv) < 2 or not str.isdigit(sys.argv[1]) else sys.argv[1]
 
@@ -22,3 +22,6 @@ if __name__ == '__main__':
     print('Serving on 0.0.0.0:' + str(port) + ' or http://localhost:' + str(port))
     serve(app, host='0.0.0.0', port=port, max_request_body_size=1073741824000000,
           max_request_header_size=85899345920000)
+
+if __name__ == '__main__':
+    main()
