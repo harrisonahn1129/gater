d65b75abde123630fcd5470b0719cd0d248e50d7
Jeremy Muhlich <jmuhlich@bitflood.org>
2025-09-11 00:05:38 -0400

    Begin work on Tk GUI

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import sys
+import threading
+import tkinter
 
 from waitress import serve
 
 from minerva_analysis import app
+
 
 def main():
     # use port 8000 if no port is specified via command line argument
@@ -19,9 +22,20 @@ def main():
         is_docker = False
     app.config['IS_DOCKER'] = is_docker
 
+    root = tkinter.Tk(baseName="Gater", className="Gater")
+    label = tkinter.Label(
+        root, text="Welcome to Gater!\nClose this window to quit.", justify="left"
+    )
+    label.pack()
+    threading.Thread(target=serve, args=(port,), daemon=True).start()
+    root.mainloop()
+
+
+def serve(port):
     print('Serving on 0.0.0.0:' + str(port) + ' or http://localhost:' + str(port))
     serve(app, host='0.0.0.0', port=port, max_request_body_size=1073741824000000,
           max_request_header_size=85899345920000)
+
 
 if __name__ == '__main__':
     main()
